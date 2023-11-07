cc51d3ec5fc9e92f877d21852c13cae93d94798a
dnunezq <dnunezq@unal.edu.co>
2023-11-06 23:42:29 -0500

    add appTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:farrap/config/router/app_router.dart';
+import 'package:farrap/presentation/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final appTheme = ref.watch( themeNotifierProvider );
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       routerConfig: appRouter,
+      theme: appTheme.getTheme(),
     );
   }
 }
