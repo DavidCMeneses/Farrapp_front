c836f349d8ce2147682abde0f2168874046bb745
dnunezq <dnunezq@unal.edu.co>
2023-11-06 23:47:03 -0500

    add provider at the main app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp()
+    )
+  );
+}
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
