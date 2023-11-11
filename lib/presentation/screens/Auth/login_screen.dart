624158c2f092a525241a78d65df39cb5d4761981
dnunezq <dnunezq@unal.edu.co>
2023-11-11 18:59:05 -0500

    add login screen

---
+++ lib/presentation/screens/Auth/login_screen.dart
@@ -0,0 +1,149 @@
+import 'dart:math';
+
+import 'package:farrap/presentation/providers/auth_provider.dart';
+import 'package:farrap/presentation/providers/login_form_provider.dart';
+import 'package:farrap/presentation/widgets/custom_text_form_field.dart';
+import 'package:farrap/presentation/widgets/user_type.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //final size = MediaQuery.of(context).size;
+    //final color = Theme.of(context).colorScheme;
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child:  Scaffold(
+        body: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox( height: 200 ),
+                  // Icon Banner
+                  const Text('Ingresar', 
+                    style: TextStyle(
+                      fontSize: 30)),
+                  
+                  const SizedBox( height: 15 ),
+                  
+                  const Text('Hola ¿Listo para pasarla genial?', 
+                    style: TextStyle(
+                      fontSize: 20)),
+                  
+                  const SizedBox( height: 15 ),
+                  
+                  Container(
+                    child: const _LoginForm(),
+                  )
+
+                  
+
+                  
+                ],
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class _LoginForm extends ConsumerWidget {
+
+  const _LoginForm();
+
+  void showSnackbar( BuildContext context, String message ) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final loginForm = ref.watch(loginFormProvider);
+
+    ref.listen(authProvider, (previous, next) {
+      if ( next.errorMessage.isEmpty ) return;
+      showSnackbar( context, next.errorMessage );
+    }); 
+
+    return Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: UserRadio(
+              type: loginForm.userType,
+              onChanged: ref.read(loginFormProvider.notifier).onUserTypeChanged,
+            )//const _LoginForm(),
+          ),
+          const SizedBox( height: 15 ),
+
+          CustomTextFormField(
+            label: 'Usuario',
+            keyboardType: TextInputType.emailAddress,
+            onChanged: ref.read(loginFormProvider.notifier).onUsernameChanged,
+            errorMessage: loginForm.isFormPosted ?
+               loginForm.userName.errorMessage 
+               : null,
+            
+          ),
+          const SizedBox( height: 30 ),
+
+          CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChanged: ref.read(loginFormProvider.notifier).onPasswordChanged,
+            errorMessage: loginForm.isFormPosted ?
+               loginForm.password.errorMessage 
+               : null,
+            
+          ),
+    
+          const SizedBox( height: 30 ),
+
+          SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: FilledButton(
+              onPressed: loginForm.isPosting
+                ? null 
+                : ref.read(loginFormProvider.notifier).onFormSubmit
+,
+              child: const Text('Ingresar'),
+            )
+              
+          ),
+
+          const SizedBox( height: 30 ),
+
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('¿No tienes cuenta?'),
+              TextButton(
+                onPressed: ()=> context.push('/register'), 
+                child: const Text('Crea una aquí')
+              ) 
+            ],
+          ),
+
+
+        ],
+      );
+  }
+}
