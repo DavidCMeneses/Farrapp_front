e23c689520142bb23e12cc0b39d54bf3b5bf8da8
dnunezq <dnunezq@unal.edu.co>
2023-11-11 18:58:59 -0500

    initial loading screen

---
+++ lib/presentation/screens/Auth/initial_loading_screen.dart
@@ -0,0 +1,41 @@
+import 'package:farrap/presentation/providers/auth_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class InitialLoadingScreen extends ConsumerWidget {
+  const InitialLoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    ref.listen(authProvider, (previous, next) {
+      if ( next.authStatus ==  AuthStatus.authenticated ) {
+        Future.delayed(Duration(seconds: 3), () {
+          context.go('/');
+        });
+      }
+      Future.delayed(Duration(seconds: 3), () {
+      context.go('/');
+    });
+    }); 
+
+    
+    final color = Theme.of(context).colorScheme;
+
+    return  Scaffold(
+      backgroundColor: color.background ,
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Column(
+          children: [
+            const SizedBox( height: 80 ),
+            Text('FARRAP', style: TextStyle(color: color.primary, fontSize: 40) ),
+            Text('SFP 2023', style: TextStyle(color: color.secondary, fontSize: 20)),
+        
+          ],
+        ),
+      ),
+    );
+  }
+}
