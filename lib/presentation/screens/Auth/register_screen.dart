5d5149e2e1d1b697624bb090f87b92d8d02e4af3
dnunezq <dnunezq@unal.edu.co>
2023-11-11 21:56:39 -0500

    Revert "test"
    This reverts commit 2479b0c5ff2d3114716545c637ed52fe46522729.

--- lib/presentation/screens/Auth/register_screen.dart
+++ lib/presentation/screens/Auth/register_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:time_range_picker/time_range_picker.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -7,21 +6,7 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: IconButton(
-            icon: Icon(Icons.access_alarm),
-            onPressed: () async {
-              TimeRange result = await showTimeRangePicker(
-                use24HourFormat: false,
-                clockRotation: 180,
-                context: context,
-              );
-              print("result " + result.toString());
-            },
-          ),
-        ));
-    
-      }
+      appBar: AppBar(title: Text('Register'),),
+      body: const Placeholder());
+  }
 }
-
