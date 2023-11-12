2479b0c5ff2d3114716545c637ed52fe46522729
dnunezq <dnunezq@unal.edu.co>
2023-11-11 21:53:54 -0500

    test

--- lib/presentation/screens/Auth/register_screen.dart
+++ lib/presentation/screens/Auth/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_range_picker/time_range_picker.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -6,7 +7,21 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Register'),),
-      body: const Placeholder());
-  }
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: IconButton(
+            icon: Icon(Icons.access_alarm),
+            onPressed: () async {
+              TimeRange result = await showTimeRangePicker(
+                use24HourFormat: false,
+                clockRotation: 180,
+                context: context,
+              );
+              print("result " + result.toString());
+            },
+          ),
+        ));
+    
+      }
 }
+
