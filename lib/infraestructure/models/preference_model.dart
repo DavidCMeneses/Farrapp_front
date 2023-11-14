d5d3a4b9a7e2c743bd0593ddad02263c36357ba5
dnunezq <dnunezq@unal.edu.co>
2023-11-13 22:47:44 -0500

    add models

---
+++ lib/infraestructure/models/preference_model.dart
@@ -0,0 +1,11 @@
+
+class PreferenceModel {
+
+  final String type;
+  final String name;
+
+  PreferenceModel({
+    required this.type, 
+    required this.name});
+
+}
---
+++ lib/infraestructure/models/schedule_model.dart
@@ -0,0 +1,14 @@
+
+class ScheduleModel {
+
+  final String day;
+  final String open;
+  final String close;
+
+  ScheduleModel({
+    required this.day, 
+    required this.open, 
+    required this.close});
+
+
+}
