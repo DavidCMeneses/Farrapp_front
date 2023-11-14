eaa35bbb830b6b776bdc830ad1ea1a51cd3eacc6
dnunezq <dnunezq@unal.edu.co>
2023-11-13 22:48:56 -0500

    refactor horizontal chip selection to provider connect

--- lib/presentation/widgets/horizontal_chip_selection.dart
+++ lib/presentation/widgets/horizontal_chip_selection.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 class ChipList extends StatefulWidget {
   final String label;
-  final Set<String> selectedChipList;
+  final List<String> selectedChipList;
   final List<String> chipText;
   final double? width;
   final double? height;
+  final Function(List<String>) onSelectChanged;
 
   const ChipList({
     super.key,
@@ -13,7 +14,8 @@ class ChipList extends StatefulWidget {
     required this.selectedChipList,
     required this.chipText,
     this.height,
-    this.width,
+    this.width, 
+    required this.onSelectChanged,
   });
 
   @override
@@ -23,63 +25,52 @@ class ChipList extends StatefulWidget {
 class _FilterChipExampleState extends State<ChipList> {
   @override
   Widget build(BuildContext context) {
-    const borderRadius = Radius.circular(15);
 
-    return SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Column(
+    return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               widget.label,
               style: const TextStyle(
                   color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12),
+                  fontWeight: FontWeight.bold),
             ),
             Container(
               height: widget.height,
               width: widget.width,
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: borderRadius,
-                      topRight: borderRadius,
-                      bottomLeft: borderRadius,
-                      bottomRight: borderRadius),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.06),
-                        blurRadius: 10,
-                        offset: const Offset(0, 5))
-                  ]),
               padding: const EdgeInsets.all(5),
-              child: Row(children: [
-                Wrap(
-                  spacing: 15.0,
-                  children: widget.chipText.map((String item) {
-                    return FilterChip(
-                      label: Text(
-                        item,
-                        style: const TextStyle(
-                            fontSize: 18, color: Colors.black54),
-                      ),
-                      selected: widget.selectedChipList.contains(item),
-                      onSelected: (bool selected) {
-                        setState(() {
-                          if (selected) {
-                            widget.selectedChipList.add(item);
-                          } else {
-                            widget.selectedChipList.remove(item);
-                          }
-                        });
-                      },
-                    );
-                  }).toList(),
-                )
-              ]),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  Wrap(
+                    spacing: 15.0,
+                    children: widget.chipText.map((String item) {
+                      return FilterChip(
+                        label: Text(
+                          item,
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.black54),
+                        ),
+                        selected: widget.selectedChipList.contains(item),
+                        onSelected: (bool selected) {
+                          List<String> newSelectedChipList = [...widget.selectedChipList];
+
+                          setState(() {
+                            if (selected) {
+                              newSelectedChipList.add(item);
+                            } else {
+                              newSelectedChipList.remove(item);
+                            }
+                            widget.onSelectChanged(newSelectedChipList);
+                          });
+                        },
+                      );
+                    }).toList(),
+                  )
+                ]),
+              ),
             ),
           ],
-        ));
+        );
   }
 }
